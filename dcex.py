0db2a7db3d79826b72a1e165d8ad64b476826e61
x1nx3r <monmega110@gmail.com>
2023-04-14 00:36:29 +0700

    adding ping command

--- dcex.py
+++ dcex.py
@@ -30,6 +30,17 @@ class Bot(commands.Bot):
 
 bot = Bot()
 
+@bot.command()
+async def ping(ctx):
+    if round(client.latency * 1000) <= 50:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x44ff44)
+    elif round(client.latency * 1000) <= 100:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xffd000)
+    elif round(client.latency * 1000) <= 200:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xff6600)
+    else:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x990000)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def play(ctx: commands.Context, *, search: str) -> None:
