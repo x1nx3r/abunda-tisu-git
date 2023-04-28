9a700d5f78229d10421c18210cf51c4ff9f26fb9
x1nx3r <monmega110@gmail.com>
2023-04-29 03:09:55 +0700

    adding plans

--- dcex.py
+++ dcex.py
@@ -32,14 +32,14 @@ bot = Bot()
 
 @bot.command()
 async def ping(ctx):
-    if round(client.latency * 1000) <= 50:
-        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x44ff44)
-    elif round(client.latency * 1000) <= 100:
-        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xffd000)
-    elif round(client.latency * 1000) <= 200:
-        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xff6600)
+    if round(bot.latency * 1000) <= 50:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x44ff44)
+    elif round(bot.latency * 1000) <= 100:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(bot.latency *1000)}** milliseconds!", color=0xffd000)
+    elif round(bot.latency * 1000) <= 200:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(bot.latency *1000)}** milliseconds!", color=0xff6600)
     else:
-        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x990000)
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x990000)
     await ctx.send(embed=embed)
 
 @bot.command()
---
+++ plans and to-do lists.md
@@ -0,0 +1,3 @@
+<h1>Function Map</h1>
+
+* dcex.py is the main script. It handles the bot launch and 
